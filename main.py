31038361759ee3f7f3aea11a0398d4217f858c6f
andreacarrarini <andrea.carrarini.18@gmail.com>
2022-01-13 16:48:50 +0100

    batched the load of data in memory

--- main.py
+++ main.py
@@ -168,29 +168,45 @@ def get_label(csv_list, file_name):
 #     return waveform, label
 
 
-def prepare_waveform_dataset(files_dataset, csv_list):
+def prepare_waveform_dataset(files_dataset, csv_list, batch_size):
     waveform_label_dataset = []
+    count = 0
 
     for file_path in files_dataset:
-        # waveform_label_couple = []
 
-        # ffff = tf.io.decode_raw(file_path, tf.uint8)
-        # dddd = tf.strings.as_string(ffff)
+        if count == batch_size:
+            break
 
         file_name = get_file_name_from_path(file_path)
 
         label = get_label(csv_list, file_name)
 
-        # audio_binary = tf.io.read_file(file_path)
-        # waveform = decode_audio(audio_binary)
-
         sound_file, sampling_rate = librosa.load(str(file_path), sr=None)
-
-        # waveform_label_couple.append((sound_file, label))
 
         waveform_label_dataset.append((sound_file, label))
 
-    return waveform_label_dataset
+        count += 1
+
+        # Removing the file from the file dataset, otherwise we will get the same batch_size elements each time
+        files_dataset.remove(file_path)
+
+    return waveform_label_dataset, files_dataset
+
+def get_all_label_types(csv_list):
+    label_types = []
+
+    for elem in csv_list:
+
+        # Skipping 1st row
+        if csv_list.index(elem) == 0:
+            continue
+
+        if elem[7] in label_types:
+            continue
+        else:
+            label_types.append(elem[7])
+
+    return label_types
 
 
 def data_generator(sound_arrays, labels):
@@ -212,7 +228,7 @@ def data_PORCODDIO(sound_arrays, labels):
 
 def data_PORCODDIO_CANE(waveform_label_structure):
     dataset_DIOCANE = []
-    # for i in range(len(waveform_label_structure)):
+    # for i in range(len(waveform_label_structure_TRAIN)):
     for i in range(3):
         audio_ragged_tensor = tf.ragged.constant(waveform_label_structure[i][0])
         # label_ragged_tensor = tf.ragged.constant(labels[i])
@@ -245,7 +261,7 @@ def get_spectrogram(waveform):
     spectrogram = tf.abs(spectrogram)
     # Add a `channels` dimension, so that the spectrogram can be used
     # as image-like input data with convolution layers (which expect
-    # shape (`batch_size`, `height`, `width`, `channels`).
+    # shape (`loading_batch_size`, `height`, `width`, `channels`).
     spectrogram = spectrogram[..., tf.newaxis]
     return spectrogram
 
@@ -282,179 +298,28 @@ def labels_to_IDs(label_tensors):
 
     return label_types
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print(tf.__version__)
-
-    # Set the seed value for experiment reproducibility.
-    seed = 42
-    tf.random.set_seed(seed)
-    np.random.seed(seed)
-
-    # Get the data
-
-    DATASET_PATH = '/Users/drugh/Documents/PycharmProjects/MSA_Project/UrbanSound8K'
-
-    DATASET_PATH = os.path.join('C:', os.sep, 'Users', 'drugh', 'Documents', 'PycharmProjects', 'MSA_Project',
-                                'UrbanSound8K')
-    print("DATASET_PATH is : " + DATASET_PATH)
-
-    data_dir = pathlib.Path(DATASET_PATH)
-    if data_dir.exists():
-        print(data_dir)
-    if not data_dir.exists():
-        print("Data_dir doesn't exist!")
-        # print(local_file_path)
-
-    # inspect.getsourcefile(tf)
-
-    # commands = np.array(tf.io.gfile.listdir(str(data_dir)))
-    # commands = commands[commands != 'README.md']
-    # print('Commands:', commands)
-
-    # train_set = tf.io.gfile.glob(str(data_dir) + '/audio/fold1/*.wav')
-    train_set = tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold1', '*.wav'))
-    # train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold2', '*.wav'))
-    # train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold3', '*.wav'))
-    # train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold4', '*.wav'))
-    # train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold6', '*.wav'))
-
-    # train_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold2/*.wav')
-    # train_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold3/*.wav')
-    # train_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold4/*.wav')
-    # train_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold6/*.wav')
-
-    # for elem in train_set:
-    #     print(elem)
-
-    # test_set = tf.io.gfile.glob(str(data_dir) + '/audio/fold5/*.wav')
-    test_set = tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold5', '*.wav'))
-    # test_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold7', '*.wav'))
-    # test_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold8', '*.wav'))
-    # test_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold9', '*.wav'))
-    # test_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold10', '*.wav'))
-
-    # test_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold7/*.wav')
-    # test_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold8/*.wav')
-    # test_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold9/*.wav')
-    # test_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold10/*.wav')
-
-    # test_file = tf.io.read_file(DATASET_PATH + '/audio/fold1/7061-6-0-0.wav')
-    test_file = tf.io.read_file(os.path.join(DATASET_PATH, 'audio', 'fold1', '7061-6-0-0.wav'))
-
-    test_audio, _ = tf.audio.decode_wav(contents=test_file)
-    test_audio.shape
-
-    AUTOTUNE = tf.data.AUTOTUNE
-
-    # Trying not to convert into a dataset
-    # files_ds = tf.data.Dataset.from_tensor_slices(train_set)
-
-    # for elem in files_ds:
-    #     print(elem)
-
-    # for elem in files_ds:
-    #     print(elem)
-
-    # CSV_PATH = '/content/drive/MyDrive/MSA/UrbanSound8K/metadata/UrbanSound8K.csv'
-    # open_metadata_file = open_csv_file(csv_filepath)
-
-    # pippo = '/content/drive/MyDrive/MSA/UrbanSound8K/metadata/UrbanSound8K.csv'
-    # parts = tf.strings.split(input=pippo,sep='/')
-    # print(parts[-1])
-
-    CSV_PATH = '/Users/drugh/Documents/PycharmProjects/MSA_Project/UrbanSound8K/metadata/UrbanSound8K.csv'
-    CSV_PATH = os.path.join('C:', os.sep, 'Users', 'drugh', 'Documents', 'PycharmProjects',
-                            'MSA_Project', 'UrbanSound8K', 'metadata', 'UrbanSound8K.csv')
-
-    global OPEN_FILE
-    OPEN_FILE = open_csv_file(CSV_PATH)
-
-    # It's a list in which each element is a row of the csv file (list of columns)
-    global csv_list
-    csv_list = csv_file_to_list(OPEN_FILE)
-
-    close_csv_file(OPEN_FILE)
-
-    # Preparing the data structure: list[(waveform, label), ...]
-    # waveform_label_structure = prepare_waveform_dataset(files_ds, csv_list)
-    waveform_label_structure = prepare_waveform_dataset(train_set, csv_list)
-
-    # waveform_ds = files_ds.map(map_func=get_waveform_and_label_2 , num_parallel_calls=AUTOTUNE)
-
-    # sound_arrays = []
-    # labels = []
-    # for elem in waveform_label_structure:
-    #     sound_arrays.append(elem[0])
-    #     label = []
-    #     for char in elem[1]:
-    #         label.append(ord(char))
-    #     labels.append(np.array(label))
-    #     # print(sound_arrays)
-    #     # print(labels)
-
+def build_dataset(waveform_label_structure):
     sound_arrays = []
     labels = []
-    for elem in waveform_label_structure:
+    for elem in waveform_label_structure_TRAIN:
         sound_arrays.append(elem[0])
         labels.append(elem[1])
 
-    max_audio_length = get_max_length(sound_arrays)
+    # max_audio_length = get_max_length(sound_arrays)
 
     sound_tensors_array = []
     for elem in sound_arrays:
         t = tf.constant(elem)
-        # asd = tf.transpose(t, [0])
-        # asd = tf.reshape(t, [1,192000])
         sound_tensor = pad_up_to_SPECIFIC(t, 384000, 0)
-        sound_tensors_array.append(tf.reshape(sound_tensor, [1,384000]))
-
-    # for i in range(100):
-    #     print(sound_tensors[i])
+        sound_tensors_array.append(tf.reshape(sound_tensor, [1, 384000]))
 
     sound_tensors = tf.data.Dataset.from_tensor_slices(sound_tensors_array)
     label_tensors = tf.data.Dataset.from_tensor_slices(labels)
     dataset = tf.data.Dataset.zip((sound_tensors, label_tensors))
 
-    # Transforming the data structure into a dataset
-    # waveform_label_dataset = tf.data.Dataset.from_tensor_slices(sound_arrays, labels)
-    # waveform_label_dataset = tf.data.Dataset.from_tensor_slices(sound_arrays)
+    return dataset, label_tensors
 
-    # waveform_label_dataset = tf.data.Dataset.from_generator(
-    #     lambda: iter(zip(sound_arrays, labels)),
-    #     output_types=(tf.float32, tf.int64),
-    #     output_signature=(
-    #         tf.TensorSpec(shape=(), dtype=tf.int32),
-    #         tf.RaggedTensorSpec(shape=(2, None), dtype=tf.int32))
-    # ).padded_batch(
-    #     batch_size=32,
-    #     padded_shapes=([None], ())
-    # )
-
-    # TODO: Understand if both sound_arrays and labels elems must be converted to tensors
-    #       and also how to pad these tensors to the same size (this seems to be the issue here)
-    # waveform_label_dataset = tf.data.Dataset.from_generator(
-    #     data_generator,
-    #     args=[sound_arrays, labels],
-    #     output_signature=(
-    #         tf.RaggedTensorSpec(shape=(None, 2), dtype=tf.float32)),
-    # )
-
-    # print("Sound Tensors is: ")
-    # print(sound_tensors)
-    # print("Label Tensors is: ")
-    # print(label_tensors)
-
-    # for label in enumerate(label_tensors.take(100)):
-    #     print(label)
-
-    # waveform_label_dataset = data_PORCODDIO(sound_arrays, labels)
-    # waveform_label_dataset = data_PORCODDIO_CANE(waveform_label_structure)
-    # print("Il Dataset è: " )
-    # print(waveform_label_dataset)
-
-    # madonnaputtana_DATASET = tf.data.Dataset(waveform_label_dataset)
-
+def plot_dataset_examples(dataset):
     rows = 4
     cols = 4
     n = rows * cols
@@ -493,8 +358,7 @@ if __name__ == '__main__':
     axes[1].set_title('Spectrogram')
     plt.show()
 
-    labels_types = labels_to_IDs(label_tensors)
-
+def build_spectrograms_dataset(dataset, labels_types):
     spectrograms = []
     labels_IDs = []
 
@@ -507,27 +371,121 @@ if __name__ == '__main__':
 
     spectrogram_tensors = tf.data.Dataset.from_tensor_slices(spectrograms)
     label_ID_tensors = tf.data.Dataset.from_tensor_slices(labels_IDs)
-    dataset_2 = tf.data.Dataset.zip((spectrogram_tensors, label_ID_tensors))
+    return tf.data.Dataset.zip((spectrogram_tensors, label_ID_tensors))
 
+def plot_spectrogram_dataset(dataset):
     rows = 3
     cols = 3
     n = rows * cols
     fig, axes = plt.subplots(rows, cols, figsize=(10, 10))
 
-    for i, (spectrogram, label_id) in enumerate(dataset_2.take(n)):
+    for i, (spectrogram, label_id) in enumerate(dataset.take(n)):
         r = i // cols
         c = i % cols
         ax = axes[r][c]
         plot_spectrogram(spectrogram.numpy(), ax)
-        ax.set_title((labels_types[label_id.numpy()]).numpy())
+        ax.set_title((labels_types[label_id.numpy()]))
         ax.axis('off')
 
     plt.show()
 
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(tf.__version__)
 
-    # for (audio, label) in dataset:
-    #     label_id = get_label_ID(label, label_tensors)
-    #     print(label_id)
+    # Set the seed value for experiment reproducibility.
+    seed = 42
+    tf.random.set_seed(seed)
+    np.random.seed(seed)
+
+    # Get the data
+
+    DATASET_PATH = '/Users/drugh/Documents/PycharmProjects/MSA_Project/UrbanSound8K'
+
+    DATASET_PATH = os.path.join('C:', os.sep, 'Users', 'drugh', 'Documents', 'PycharmProjects', 'MSA_Project',
+                                'UrbanSound8K')
+    print("DATASET_PATH is : " + DATASET_PATH)
+
+    data_dir = pathlib.Path(DATASET_PATH)
+    if data_dir.exists():
+        print(data_dir)
+    if not data_dir.exists():
+        print("Data_dir doesn't exist!")
+
+    # train_set = tf.io.gfile.glob(str(data_dir) + '/audio/fold1/*.wav')
+    train_set = tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold1', '*.wav'))
+    # train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold2', '*.wav'))
+    # train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold3', '*.wav'))
+    # train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold4', '*.wav'))
+    # train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold6', '*.wav'))
+
+    # train_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold2/*.wav')
+    # train_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold3/*.wav')
+    # train_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold4/*.wav')
+    # train_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold6/*.wav')
+
+    # test_set = tf.io.gfile.glob(str(data_dir) + '/audio/fold5/*.wav')
+    test_set = tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold5', '*.wav'))
+    # test_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold7', '*.wav'))
+    # test_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold8', '*.wav'))
+    # test_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold9', '*.wav'))
+    # test_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep,'audio', 'fold10', '*.wav'))
+
+    # test_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold7/*.wav')
+    # test_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold8/*.wav')
+    # test_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold9/*.wav')
+    # test_set += tf.io.gfile.glob(str(data_dir) + '/audio/fold10/*.wav')
+
+    # test_file = tf.io.read_file(DATASET_PATH + '/audio/fold1/7061-6-0-0.wav')
+    test_file = tf.io.read_file(os.path.join(DATASET_PATH, 'audio', 'fold1', '7061-6-0-0.wav'))
+
+    # test_audio, _ = tf.audio.decode_wav(contents=test_file)
+    # test_audio.shape
+
+    AUTOTUNE = tf.data.AUTOTUNE
+
+    CSV_PATH = '/Users/drugh/Documents/PycharmProjects/MSA_Project/UrbanSound8K/metadata/UrbanSound8K.csv'
+    CSV_PATH = os.path.join('C:', os.sep, 'Users', 'drugh', 'Documents', 'PycharmProjects',
+                            'MSA_Project', 'UrbanSound8K', 'metadata', 'UrbanSound8K.csv')
+
+    global OPEN_FILE
+    OPEN_FILE = open_csv_file(CSV_PATH)
+
+    # It's a list in which each element is a row of the csv file (list of columns)
+    global csv_list
+    csv_list = csv_file_to_list(OPEN_FILE)
+
+    close_csv_file(OPEN_FILE)
+
+    # IMPORTANT TO AVOID LOADING IN MEMORY ALL DATASET AT THE SAME TIME (currently trying batch_size * 4)
+    loading_batch_size = 256
+
+    # Preparing the data structure: list[(waveform, label), ...]
+    waveform_label_structure_TRAIN, train_set = prepare_waveform_dataset(train_set, csv_list, loading_batch_size)
+    waveform_label_structure_TEST, test_set = prepare_waveform_dataset(test_set, csv_list, loading_batch_size)
+
+    TRAIN_dataset, label_tensors = build_dataset(waveform_label_structure_TRAIN)
+
+    TEST_dataset, non_serve = build_dataset(waveform_label_structure_TEST)
+
+    plot_dataset_examples(TRAIN_dataset)
+
+    labels_types = get_all_label_types(csv_list)
+
+    # TRAIN_dataset_spectrograms = build_spectrograms_dataset(TRAIN_dataset, labels_types)
+    #
+    # TEST_dataset_spectrograms = build_spectrograms_dataset(TEST_dataset, labels_types)
+
+    TRAIN_dataset = build_spectrograms_dataset(TRAIN_dataset, labels_types)
+
+    TEST_dataset = build_spectrograms_dataset(TEST_dataset, labels_types)
+
+    plot_spectrogram_dataset(TRAIN_dataset)
+
+    # TODO: I'm here
+    batch_size = 64
+    train_ds = TRAIN_dataset.batch(batch_size)
+    val_ds = TEST_dataset.batch(batch_size)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
