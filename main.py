4b25119cdd53f6cd190732952c1b0ba459a83e75
andreacarrarini <andrea.carrarini.18@gmail.com>
2022-03-17 19:41:38 +0100

    half MFCC training done, next: ResNet50 and VGG16

--- main.py
+++ main.py
@@ -18,13 +18,12 @@ def build_all_spectrograms_datasets(train_ds, val_ds, test_ds, labels_types, is_
     return VALIDATION_dataset, TEST_dataset
 
 
-def build_all_MFCC_datasets(train_ds, val_ds, test_ds, labels_types, is_RNN):
+def build_all_MFCC_datasets(train_ds, val_ds, labels_types, is_RNN):
     TRAIN_dataset = fn.build_MFCC_dataset(train_ds, labels_types, is_RNN)
     VALIDATION_dataset = fn.build_MFCC_dataset(val_ds, labels_types, is_RNN)
-    TEST_dataset = fn.build_MFCC_dataset(test_ds, labels_types, is_RNN)
-    return TRAIN_dataset, VALIDATION_dataset, TEST_dataset
+    return TRAIN_dataset, VALIDATION_dataset
 
-def Train_MFCC():
+def Train_MFCC(model_name):
 
     # Set the seed value for experiment reproducibility.
     seed = 42
@@ -44,181 +43,206 @@ def Train_MFCC():
     if not data_dir.exists():
         print("Data_dir doesn't exist!")
 
-    train_set = tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold1', '*.wav'))
-    train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold2', '*.wav'))
-    train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold3', '*.wav'))
-    train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold4', '*.wav'))
-    train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold6', '*.wav'))
-
-    possible_test_set = ['5', '7', '8', '9', '10']
-
     results = []
 
     feature_name = "MFCC"
 
-    # For the 4 architectures chosen
-    for k in range(4):
+    CSV_PATH = os.path.join('C:', os.sep, 'Users', 'drugh', 'Documents', 'PycharmProjects',
+                            'MSA_Project', 'UrbanSound8K', 'metadata', 'UrbanSound8K.csv')
 
-        global model_name
+    global OPEN_FILE, input_shape, model
+    OPEN_FILE = fn.open_csv_file(CSV_PATH)
 
-        # Cycling over hyperparameters
-        learning_rates = [0.1, 0.01, 0.001, 0.0001]
-        EPOCHS = 50
-        for j in learning_rates:
-            LEARNING_RATE = j
+    # It's a list in which each element is a row of the csv file (list of columns)
+    global csv_list
+    csv_list = fn.csv_file_to_list(OPEN_FILE)
 
-            folders_in_train_set = 2
-            for i in range(folders_in_train_set):
+    print("Total number of files in the dataset: " + str(len(csv_list)))
 
-                # CROSS-VALIDATION
-                extracted_test_set = possible_test_set[i]
-                folder_name = "fold" + extracted_test_set
-                test_set = tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', folder_name, '*.wav'))
+    fn.close_csv_file(OPEN_FILE)
 
-                possible_val_set = possible_test_set.copy()
-                possible_val_set.remove(extracted_test_set)
+    labels_types = fn.get_all_label_types(csv_list)
 
-                extracted_val_set = possible_test_set[random.randint(0, folders_in_train_set - 1)]
-                folder_name = "fold" + extracted_val_set
-                validation_set = tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', folder_name, '*.wav'))
+    # loading_batch_size = 256
+    # loading_batch_size = 512
+    # loading_batch_size = 1024
+    # loading_batch_size = 2048
+    loading_batch_size = 4096
+    # loading_batch_size = 8192
+    # loading_batch_size = 99999
 
-                CSV_PATH = os.path.join('C:', os.sep, 'Users', 'drugh', 'Documents', 'PycharmProjects',
-                                        'MSA_Project', 'UrbanSound8K', 'metadata', 'UrbanSound8K.csv')
+    # if model_name == "CNN_2":
+    #     loading_batch_size = 256
+    # elif model_name == "VGG16":
+    #     loading_batch_size = 256
 
-                global OPEN_FILE, input_shape, model
-                OPEN_FILE = fn.open_csv_file(CSV_PATH)
+    # TEST_SET doesn't change in cross validation
+    test_set = tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold5', '*.wav'))
+    test_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold7', '*.wav'))
+    test_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold8', '*.wav'))
+    test_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold9', '*.wav'))
+    test_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold10', '*.wav'))
 
-                # It's a list in which each element is a row of the csv file (list of columns)
-                global csv_list
-                csv_list = fn.csv_file_to_list(OPEN_FILE)
+    waveform_label_structure_TEST, test_set = fn.prepare_waveform_dataset(test_set, csv_list, loading_batch_size)
 
-                print("Total number of files in the dataset: " + str(len(csv_list)))
+    # Sould be only for Spectrogram
+    # TEST_dataset, non_serve = fn.build_dataset(waveform_label_structure_TEST)
 
-                fn.close_csv_file(OPEN_FILE)
+    TEST_dataset = fn.build_MFCC_dataset(waveform_label_structure_TEST, labels_types, False)
 
-                # IMPORTANT TO AVOID LOADING IN MEMORY ALL DATASET AT THE SAME TIME (currently trying batch_size * 4)
-                # loading_batch_size = 256
-                # loading_batch_size = 512
-                # loading_batch_size = 1024
-                loading_batch_size = 2048
-                # loading_batch_size = 4096
-                # loading_batch_size = 8192
-                # loading_batch_size = 99999
+    print("1")
 
-                # Preparing the data structure: list[(waveform, label), ...]
-                waveform_label_structure_TRAIN, train_set = fn.prepare_waveform_dataset(train_set, csv_list, loading_batch_size)
-                waveform_label_structure_TEST, test_set = fn.prepare_waveform_dataset(test_set, csv_list, loading_batch_size)
-                waveform_label_structure_VALIDATION, validation_set = fn.prepare_waveform_dataset(validation_set, csv_list,
-                                                                                                  loading_batch_size)
+    # Cycling over hyperparameters
+    learning_rates = [0.1, 0.01, 0.001, 0.0001]
+    EPOCHS = 50
+    for j in learning_rates:
+        LEARNING_RATE = j
 
-                labels_types = fn.get_all_label_types(csv_list)
+        # CROSS-VALIDATION
+        possible_val_set = ['1', '2', '3', '4', '6']
+        for pippo in range(5):
+            extracted_val_set = possible_val_set[pippo]
+            folder_name = "fold" + extracted_val_set
+            validation_set = tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', folder_name, '*.wav'))
 
-                # For CNNs/VGG16/ResNet50 - MFCC
-                TRAIN_dataset, VALIDATION_dataset, TEST_dataset = build_all_MFCC_datasets(
-                        waveform_label_structure_TRAIN, waveform_label_structure_VALIDATION,
-                        waveform_label_structure_TEST, labels_types, False)
+            global train_set
 
-                batch_size = 64
-                train_ds = TRAIN_dataset.batch(batch_size)
-                val_ds = VALIDATION_dataset.batch(batch_size)
+            if pippo == 0:
+                train_set = tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold2', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold3', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold4', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold6', '*.wav'))
+            elif pippo == 1:
+                train_set = tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold1', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold3', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold4', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold6', '*.wav'))
+            elif pippo == 2:
+                train_set = tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold1', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold2', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold4', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold6', '*.wav'))
+            elif pippo == 3:
+                train_set = tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold1', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold2', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold3', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold6', '*.wav'))
+            elif pippo == 4:
+                train_set = tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold1', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold2', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold3', '*.wav'))
+                train_set += tf.io.gfile.glob(str(data_dir) + os.path.join(os.sep, 'audio', 'fold4', '*.wav'))
 
-                AUTOTUNE = tf.data.AUTOTUNE
+            # Preparing the data structure: list[(waveform, label), ...]
+            waveform_label_structure_TRAIN, train_set = fn.prepare_waveform_dataset(train_set, csv_list, loading_batch_size)
+            waveform_label_structure_VALIDATION, validation_set = fn.prepare_waveform_dataset(validation_set, csv_list,
+                                                                                              loading_batch_size)
 
-                train_ds = train_ds.cache().prefetch(AUTOTUNE)
-                val_ds = val_ds.cache().prefetch(AUTOTUNE)
+            # For CNNs/VGG16/ResNet50 - MFCC
+            TRAIN_dataset, VALIDATION_dataset = build_all_MFCC_datasets(
+                    waveform_label_structure_TRAIN, waveform_label_structure_VALIDATION, labels_types, False)
 
-                for mfcc, _ in TRAIN_dataset.take(1):
-                    input_shape = mfcc.shape
-                print('Input shape:', input_shape)
-                num_labels = len(labels_types)
+            batch_size = 64
+            train_ds = TRAIN_dataset.batch(batch_size)
+            val_ds = VALIDATION_dataset.batch(batch_size)
 
-                if k == 0:
-                    model = arch.build_CNN(num_labels, train_ds, input_shape)
-                    model_name = "CNN_1"
-                elif k == 1:
-                    model = arch.build_CNN_2(num_labels, train_ds, input_shape)
-                    model_name = "CNN_2"
-                elif k == 2:
-                    model = arch.build_ResNet50(num_labels, train_ds, input_shape)
-                    model_name = "ResNet_50"
-                elif k == 3:
-                    model = arch.build_VGG16(num_labels, train_ds, input_shape)
-                    model_name = "VGG16"
+            AUTOTUNE = tf.data.AUTOTUNE
 
-                model.build(input_shape)
-                model.summary()
+            train_ds = train_ds.cache().prefetch(AUTOTUNE)
+            val_ds = val_ds.cache().prefetch(AUTOTUNE)
 
-                # Adam's (Neon Genesis Evangelion) model optimization
+            for mfcc, _ in TRAIN_dataset.take(1):
+                input_shape = mfcc.shape
+            print('Input shape:', input_shape)
+            num_labels = len(labels_types)
 
-                # CNN
-                model.compile(
-                    optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE, beta_1=0.9, beta_2=0.999, epsilon=1e-07,
-                                                       amsgrad=False, name='Adam'),
-                    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                    metrics=['accuracy'],
-                )
+            patience = 5
 
-                history = model.fit(
-                    train_ds,
-                    validation_data=val_ds,
-                    epochs=EPOCHS,
-                    callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=10),
-                )
+            if model_name == "CNN_1":
+                model = arch.build_CNN(num_labels, train_ds, input_shape)
+            elif model_name == "CNN_2":
+                model = arch.build_CNN_2(num_labels, train_ds, input_shape)
+                # patience = 2
+            elif model_name == "ResNet_50":
+                model = arch.build_ResNet50(num_labels, train_ds, input_shape)
+            elif model_name == "VGG16":
+                model = arch.build_VGG16(num_labels, train_ds, input_shape)
+                # patience = 2
 
-                metrics = history.history
-                plt.plot(history.epoch, metrics['loss'], metrics['val_loss'])
-                plt.legend(['loss', 'val_loss'])
+            model.build(input_shape)
+            model.summary()
 
-                # Save plot as a .png
-                plt.savefig('tr_loss-val_loss_' + model_name + '_' + feature_name + '_lr' + str(LEARNING_RATE) +
-                            '_ep' + str(EPOCHS) + "_part-" + str(i) + '.png', dpi=300, bbox_inches='tight')
+            # Adam's (Neon Genesis Evangelion) model optimization
 
-                plt.show()
+            # CNN
+            model.compile(
+                optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE, beta_1=0.9, beta_2=0.999, epsilon=1e-07,
+                                                   amsgrad=False, name='Adam'),
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'],
+            )
 
-                # Evaluating the model performance
-                test_audio = []
-                test_labels = []
+            history = model.fit(
+                train_ds,
+                validation_data=val_ds,
+                epochs=EPOCHS,
+                callbacks=tf.keras.callbacks.EarlyStopping(verbose=1, patience=10),
+            )
 
-                for audio, label in TEST_dataset:
-                    test_audio.append(audio.numpy())
-                    test_labels.append(label.numpy())
+            metrics = history.history
+            plt.plot(history.epoch, metrics['loss'], metrics['val_loss'])
+            plt.legend(['loss', 'val_loss'])
 
-                test_audio = np.array(test_audio)
-                test_labels = np.array(test_labels)
+            # Save plot as a .png
+            plt.savefig('tr_loss-val_loss_' + model_name + '_' + feature_name + '_lr' + str(LEARNING_RATE) +
+                        '_ep' + str(EPOCHS) + "_part-" + str(pippo) + '.png', dpi=300, bbox_inches='tight')
 
-                y_pred = np.argmax(model.predict(test_audio), axis=1)
-                y_true = test_labels
+            plt.show()
 
-                test_acc = sum(y_pred == y_true) / len(y_true)
-                print(f'Test set accuracy: {test_acc:.0%}')
+            # Evaluating the model performance
+            test_audio = []
+            test_labels = []
 
-                # Confusion matrix
-                confusion_mtx = tf.math.confusion_matrix(y_true, y_pred)
-                plt.figure(figsize=(10, 8))
-                sns.heatmap(confusion_mtx,
-                            xticklabels=labels_types,
-                            yticklabels=labels_types,
-                            annot=True, fmt='g')
-                plt.xlabel('Prediction')
-                plt.ylabel('Label')
+            for audio, label in TEST_dataset:
+                test_audio.append(audio.numpy())
+                test_labels.append(label.numpy())
 
-                # Save plot as a .png
-                plt.savefig('confusion_' + model_name + '_' + feature_name + '_lr' + str(LEARNING_RATE) + '_ep' +
-                            str(EPOCHS) + "_part-" + str(i) + '.png', dpi=300, bbox_inches='tight')
+            test_audio = np.array(test_audio)
+            test_labels = np.array(test_labels)
 
-                plt.show()
+            y_pred = np.argmax(model.predict(test_audio), axis=1)
+            y_true = test_labels
 
-                train_acc = history.history['accuracy']
-                train_loss = history.history['loss']
-                val_acc = history.history['val_accuracy']
-                val_loss = history.history['val_loss']
+            test_acc = sum(y_pred == y_true) / len(y_true)
+            print(f'Test set accuracy: {test_acc:.0%}')
 
-                single_execution_results = [model_name, "MFCC", EPOCHS, LEARNING_RATE, fn.truncate(train_acc[-1],3),
-                                            fn.truncate(train_loss[-1],3), fn.truncate(val_acc[-1],3), fn.truncate(val_loss[-1],3),
-                                            fn.truncate(test_acc,3)]
-                print(single_execution_results)
+            # Confusion matrix
+            confusion_mtx = tf.math.confusion_matrix(y_true, y_pred)
+            plt.figure(figsize=(10, 8))
+            sns.heatmap(confusion_mtx,
+                        xticklabels=labels_types,
+                        yticklabels=labels_types,
+                        annot=True, fmt='g')
+            plt.xlabel('Prediction')
+            plt.ylabel('Label')
 
-                results.append(single_execution_results)
+            # Save plot as a .png
+            plt.savefig('confusion_' + model_name + '_' + feature_name + '_lr' + str(LEARNING_RATE) + '_ep' +
+                        str(EPOCHS) + "_part-" + str(pippo) + '.png', dpi=300, bbox_inches='tight')
+
+            plt.show()
+
+            train_acc = history.history['accuracy']
+            train_loss = history.history['loss']
+            val_acc = history.history['val_accuracy']
+            val_loss = history.history['val_loss']
+
+            single_execution_results = [model_name, "MFCC", EPOCHS, LEARNING_RATE, fn.truncate(train_acc[-1],3),
+                                        fn.truncate(train_loss[-1],3), fn.truncate(val_acc[-1],3), fn.truncate(val_loss[-1],3),
+                                        fn.truncate(test_acc,3)]
+            print(single_execution_results)
+
+            results.append(single_execution_results)
 
         # Writing results
         header = ['model', 'feature', 'epochs', 'learning_rate', 'train_accuracy',
@@ -638,8 +662,9 @@ if __name__ == '__main__':
     # results = Train_Spectrogram("CNN_1")
     # results = Train_Spectrogram("CNN_2")
     # results = Train_Spectrogram("ResNet_50")
-    results = Train_Spectrogram("VGG16")
+    # results = Train_Spectrogram("VGG16")
     # print(results)
+
     #
     # header = ['model', 'feature', 'epochs', 'learning_rate', 'train_accuracy',
     #           'train_loss', 'val_accuracy', 'val_loss', 'test_accuracy']
@@ -659,7 +684,14 @@ if __name__ == '__main__':
 
     # ----------------------------MFCC----------------------------------------------------------------------------------
 
-    # feature_name = "MFCC"
+    feature_name = "MFCC"
+
+    results = Train_MFCC("CNN_1")
+    results = Train_MFCC("CNN_2")
+    results = Train_MFCC("ResNet_50")
+    results = Train_MFCC("VGG16")
+    print(results)
+
     #
     # results = Train_MFCC()
     # print(results)
